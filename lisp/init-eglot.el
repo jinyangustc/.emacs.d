00f15e6b1106986fff6f2f38e0c3c2e07938775c
Jinyang Li <jinyang7@illinois.edu>
2026-01-16 08:45:14 -0600

    Add `dumb-jump`

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -5,5 +5,15 @@
 (use-package eglot)
 (use-package consult-eglot)
 
+(use-package dumb-jump
+  :init
+  (setq xref-show-definitions-function #'xref-show-definitions-completing-read)
+
+  :custom
+  (xref-search-program 'ripgrep)
+
+  :config
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
+
 (provide 'init-eglot)
 ;;; init-eglot.el ends here
