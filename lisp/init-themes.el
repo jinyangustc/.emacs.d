69a2f42453e8385b12c7ecb68c096719bff637cf
Jinyang Li <jinyang7@illinois.edu>
2026-01-16 11:46:39 -0600

    Add spacious padding

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -11,6 +11,28 @@
   (set-face-attribute 'variable-pitch nil :family "Aporetic Serif" :height 1.0)
   (set-face-attribute 'fixed-pitch nil :family "Aporetic Serif Mono" :height 1.0))
 
+
+(use-package spacious-padding
+  :ensure t
+  :config
+  ;; These are the default values, but I keep them here for visibility.
+  ;; Also check `spacious-padding-subtle-frame-lines'.
+  (setq spacious-padding-subtle-frame-lines t)
+  (setq spacious-padding-widths
+        '( :internal-border-width 15
+           :header-line-width 2
+           :mode-line-width 2
+           :custom-button-width 3
+           :tab-width 4
+           :right-divider-width 30
+           :scroll-bar-width 8
+           :fringe-width 8))
+
+  (spacious-padding-mode 1)
+
+  ;; Set a key binding if you need to toggle spacious padding.
+  (define-key global-map (kbd "<f8>") #'spacious-padding-mode))
+
 ;; (use-package modus-themes
 ;;   :demand t
 ;;   :init
