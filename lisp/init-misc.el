f6e96c2e3e7c719a7bc97115a3d41fe717e2b23a
Jinyang Li <jinyang7@illinois.edu>
2026-02-02 16:26:55 -0600

    Some regex improvements

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -39,5 +39,64 @@
 (setq initial-major-mode 'fundamental-mode
       initial-scratch-message nil)
 
+(use-package visual-regexp
+  :bind (("C-c r" . vr/replace)
+         ("C-c q" . vr/query-replace)
+         ("C-c m" . vr/mc-mark)))
+
+
+;; Bridging Re-Builder And Query-Replace-Regexp
+;; See:
+;; https://lmno.lol/alvaro/hey-mouse-dont-mess-with-my-emacs-font-size
+;;
+;; if you want to insert a newline in the regexp-builder buffer you
+;; can now use C-q C-j
+
+(defvar my/re-builder-positions nil
+      "Store point and region bounds before calling re-builder")
+    (advice-add 're-builder
+                :before
+                (defun my/re-builder-save-state (&rest _)
+                  "Save into `my/re-builder-positions' the point and region
+  positions before calling `re-builder'."
+                            (setq my/re-builder-positions
+                                  (cons (point)
+                                        (when (region-active-p)
+                                          (list (region-beginning)
+                                                (region-end)))))))
+  (defun reb-replace-regexp (&optional delimited)
+    "Run `query-replace-regexp' with the contents of re-builder. With
+  non-nil optional argument DELIMITED, only replace matches
+  surrounded by word boundaries."
+    (interactive "P")
+    (reb-update-regexp)
+    (let* ((re (reb-target-value 'reb-regexp))
+           (replacement (query-replace-read-to
+                         re
+                         (concat "Query replace"
+                                 (if current-prefix-arg
+                                     (if (eq current-prefix-arg '-) " backward" " word")
+                                   "")
+                                 " regexp"
+                                 (if (with-selected-window reb-target-window
+                                       (region-active-p)) " in region" ""))
+                         t))
+           (pnt (car my/re-builder-positions))
+           (beg (cadr my/re-builder-positions))
+           (end (caddr my/re-builder-positions)))
+      (with-selected-window reb-target-window
+        (goto-char pnt) ; replace with (goto-char (match-beginning 0)) if you want
+                        ; to control where in the buffer the replacement starts
+                        ; with re-builder
+        (setq my/re-builder-positions nil)
+        (reb-quit)
+        (query-replace-regexp re replacement delimited beg end))))
+
+(with-eval-after-load 're-builder
+  (define-key reb-mode-map (kbd "RET") #'reb-replace-regexp)
+  (define-key reb-lisp-mode-map (kbd "RET") #'reb-replace-regexp))
+
+(global-set-key (kbd "C-M-%") #'re-builder)
+
 (provide 'init-misc)
 ;;; init-misc.el ends here
