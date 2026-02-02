ab033a706627f619a5ae9ba97f0312aaa5b6eef5
Jinyang Li <jinyang7@illinois.edu>
2026-02-02 15:35:44 -0600

    Switch to sane re syntax

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -32,6 +32,7 @@
   (setq-default regex-tool-backend 'perl))
 
 (with-eval-after-load 're-builder
+  (setq reb-re-syntax 'string)
   ;; Support a slightly more idiomatic quit binding in re-builder
   (define-key reb-mode-map (kbd "C-c C-k") 'reb-quit))
 
